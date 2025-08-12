e8dc44dcbb2b20fa260670083a847e5a5be40b78
vaishnavidhotare <vaishnavi191126@gmail.com>
2025-08-12 13:46:40 +0530

    vaishnavi

---
+++ app/src/main/java/com/example/fitlife_fitcare/Abs_inter_10.java
@@ -0,0 +1,88 @@
+package com.example.fitlife_fitcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Abs_inter_10 extends AppCompatActivity {
+
+    private TextView timerText;
+    private ProgressBar progressBar;
+    private ImageButton playPauseButton;
+    private ImageButton nextButton;
+    private CountDownTimer countDownTimer;
+    private boolean isRunning = true;
+    private long timeLeft = 30000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_abs_inter10);
+
+        timerText = findViewById(R.id.abs_intertime10);
+        progressBar = findViewById(R.id.abs_interprogressBar10);
+        playPauseButton = findViewById(R.id.abs_interPauseButton10);
+        nextButton = findViewById(R.id.abs_interNextButton10);
+        progressBar.setMax(30);
+        startTimer();
+
+        playPauseButton.setOnClickListener(v -> {
+            if (isRunning) {
+                pauseTimer();
+            } else {
+                resumeTimer();
+            }
+        });
+
+        nextButton.setOnClickListener(v -> {
+
+            countDownTimer.cancel();
+            isRunning = false;
+            Intent intent = new Intent(Abs_inter_10.this, Finish.class);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timeLeft = millisUntilFinished;
+                int seconds = (int) millisUntilFinished / 1000;
+                timerText.setText("00:" + String.format("%02d", seconds));
+                progressBar.setProgress(30 - seconds);
+            }
+
+            public void onFinish() {
+                timerText.setText("00:00");
+                Intent intent1 = new Intent(Abs_inter_10.this, Finish.class);
+                startActivity(intent1);
+
+            }
+        }.start();
+        isRunning = true;
+        playPauseButton.setImageResource(android.R.drawable.ic_media_pause);
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        isRunning = false;
+        playPauseButton.setImageResource(android.R.drawable.ic_media_play);
+    }
+
+    private void resumeTimer() {
+        startTimer();
+    }
+}
---
+++ app/src/main/java/com/example/fitlife_fitcare/Abs_inter_5.java
@@ -0,0 +1,88 @@
+package com.example.fitlife_fitcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Abs_inter_5 extends AppCompatActivity {
+
+    private TextView timerText;
+    private ProgressBar progressBar;
+    private ImageButton playPauseButton;
+    private ImageButton nextButton;
+    private CountDownTimer countDownTimer;
+    private boolean isRunning = true;
+    private long timeLeft = 30000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_abs_inter5);
+
+        timerText = findViewById(R.id.abs_intertime5);
+        progressBar = findViewById(R.id.abs_interprogressBar5);
+        playPauseButton = findViewById(R.id.abs_interPauseButton5);
+        nextButton = findViewById(R.id.abs_interNextButton5);
+        progressBar.setMax(30);
+        startTimer();
+
+        playPauseButton.setOnClickListener(v -> {
+            if (isRunning) {
+                pauseTimer();
+            } else {
+                resumeTimer();
+            }
+        });
+
+        nextButton.setOnClickListener(v -> {
+
+            countDownTimer.cancel();
+            isRunning = false;
+            Intent intent = new Intent(Abs_inter_5.this, Abs_inter_6.class);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timeLeft = millisUntilFinished;
+                int seconds = (int) millisUntilFinished / 1000;
+                timerText.setText("00:" + String.format("%02d", seconds));
+                progressBar.setProgress(30 - seconds);
+            }
+
+            public void onFinish() {
+                timerText.setText("00:00");
+                Intent intent1 = new Intent(Abs_inter_5.this, Abs_inter_6.class);
+                startActivity(intent1);
+
+            }
+        }.start();
+        isRunning = true;
+        playPauseButton.setImageResource(android.R.drawable.ic_media_pause);
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        isRunning = false;
+        playPauseButton.setImageResource(android.R.drawable.ic_media_play);
+    }
+
+    private void resumeTimer() {
+        startTimer();
+    }
+}
