2541d4b7fbb549c240481208747241a3d44a0336
Harshda8607 <harshdasutar867@gmail.com>
2025-08-12 09:58:34 +0530

    qwe

--- app/src/main/java/com/example/fitlife_fitcare/Lowerbody_11.java
+++ app/src/main/java/com/example/fitlife_fitcare/Lowerbody_11.java
@@ -1,6 +1,11 @@
 package com.example.fitlife_fitcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,75 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class Lowerbody_11 extends AppCompatActivity {
+
+    private TextView timerText;
+    private ProgressBar progressBar;
+    private ImageButton playPauseButton;
+    ImageButton nextButton;
+    private CountDownTimer countDownTimer;
+    GifImageView img1;
+    private boolean isRunning = true;
+    private long timeLeft = 30000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lowerbody11);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_lowerbody12);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        timerText = findViewById(R.id.lowerbodytime11);
+        progressBar = findViewById(R.id.lowerprogressBar11);
+        playPauseButton = findViewById(R.id.lowerplayPauseButton11);
+        nextButton = findViewById(R.id.lowerNextButton11);
+        progressBar.setMax(30);
+        startTimer();
+
+        playPauseButton.setOnClickListener(v -> {
+            if (isRunning) {
+                pauseTimer();
+            } else {
+                resumeTimer();
+            }
         });
+        nextButton.setOnClickListener(v -> {
+
+            countDownTimer.cancel();
+            isRunning = false;
+            Intent intent = new Intent(Lowerbody_11.this, Lowerbody_12.class);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timeLeft = millisUntilFinished;
+                int seconds = (int) millisUntilFinished / 1000;
+                timerText.setText("00:" + String.format("%02d", seconds));
+                progressBar.setProgress(30 - seconds);
+            }
+
+            public void onFinish() {
+                timerText.setText("00:00");
+                Intent intent=new Intent(Lowerbody_11.this,Lowerbody_12.class);
+                startActivity(intent);
+            }
+        }.start();
+        isRunning = true;
+        playPauseButton.setImageResource(android.R.drawable.ic_media_pause);
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        isRunning = false;
+        playPauseButton.setImageResource(android.R.drawable.ic_media_play);
+    }
+
+    private void resumeTimer() {
+
+        startTimer();
     }
 }
